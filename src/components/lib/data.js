c6a880e5b9578281fb52f93ac836b7c14c6ed4d4
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-15 01:59:03 +0700

    Recreate files

---
+++ src/components/lib/data.js
@@ -0,0 +1,18 @@
+const data = {
+    date: {
+        day: '31',
+        month: '7',
+        year: '2024'
+    },
+    boy: {
+        name: 'Khoa Nguyen',
+        dob: '12/04/2005',
+        zodiac: 'Aries',
+    },
+    girl: {
+        name: 'Chips',
+        dob: '14/11/2005',
+        zodiac: 'Scorpio',
+    }
+}
+export default data;
